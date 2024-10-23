411a115e4a439660ccc34b34efe38a6bdf3c68be
Georgi Kolev <102030063+GeorgiKolev2006@users.noreply.github.com>
2024-10-23 19:27:56 +0300

    Created animation tree
    Base of the animation tree

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
+@onready var anim_tree = $anim_tree
+@onready var anim_state = anim_tree.get("parameters/playback")
+
 var input_movement = Vector2.ZERO
 var speed = 70
 
 func _ready():
-	$BasicSword/CollisionShape2D.disabled = true
+	$sword/CollisionShape2D.disabled = true
 
 
 func _physics_process(delta):
@@ -14,9 +17,15 @@ func move():
 	input_movement = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	if input_movement != Vector2.ZERO:
+		anim_tree.set("parameters/Idle/blend_position", input_movement)
+		anim_tree.set("parameters/Walk/blend_position", input_movement)
+		anim_tree.set("parameters/Sword/blend_position", input_movement)
+		anim_tree.set("parameters/Jump/blend_position", input_movement)
+		anim_state.travel("Walk")
 		velocity = input_movement * speed
 	
 	if input_movement == Vector2.ZERO:
+		anim_state.travel("Idle")
 		velocity = Vector2.ZERO
 		
 	move_and_slide()
