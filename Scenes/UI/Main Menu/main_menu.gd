be1d62b5448f12c92c59a7b4339da1feeb002da1
Georgi Kolev <102030063+GeorgiKolev2006@users.noreply.github.com>
2024-12-11 23:16:58 +0200

    Main menu fix, enemy directions

--- Scenes/UI/Main Menu/main_menu.gd
+++ Scenes/UI/Main Menu/main_menu.gd
@@ -1,21 +1,28 @@
 extends Control
 
+@onready var settings1: = preload("res://Scenes/UI/Main Menu/settings.tscn")
+@onready var settings: TabContainer = $Settings
+
+
 func _ready() -> void:
 	$VBoxContainer/Start.grab_focus()
 
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Levels/main_level.tscn")
 
-
-func _on_options_pressed() -> void:
-	var options_scene = load("res://Scenes/UI/Options/options.tscn").instantiate()
-	add_child(options_scene)
-	options_scene.set_position(Vector2.ZERO) 
-
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_options_pressed() -> void:
+	$VBoxContainer.hide()
+	if settings == null:
+		var settings_scene = load("res://Scenes/UI/Main Menu/settings.tscn").instantiate()
+		settings = settings_scene.get_node("Settings")
+		add_child(settings_scene)
+	settings.show()
+	settings.current_tab = 0
+	settings.get_tab_at_index(settings.current_tab).grab_focus()
--- Scripts/Enemy/enemy_mover.gd
+++ Scripts/Enemy/enemy_mover.gd
@@ -2,17 +2,18 @@ extends CharacterBody2D
 class_name enemy_movement
 
 var current_states = enemy_states.MOVEDOWN
-enum enemy_states {MOVERIGHT, MOVELEFT, MOVEUP, MOVEDOWN, DEAD}
+enum enemy_states {MOVERIGHT, MOVELEFT, MOVEUP, MOVEDOWN, DEAD, CHASE}
 
 @onready var dead_anim = preload("res://Scenes/Effects/dead_fx.tscn")
 @onready var coin_loot = preload("res://Scenes/Interactables/coin.tscn")
 @onready var hitbox_area = $EnemyHitbox
 @onready var attack_area = $EnemyAttack
-@onready var player = get_node("Player")
+@onready var player = get_tree().get_root().get_node("Player")
 @onready var navigation = get_parent().get_node("Navigation2D")
-@export var speed = 10
+@export var detection_radius = 200.0
+@export var speed = 30
 @export var health = 3
-var dir
+var dir = Vector2.ZERO
 
 func _ready() -> void:
 	random_generation()
@@ -20,6 +21,12 @@ func _ready() -> void:
 func _physics_process(delta):
 	if health <= 0:
 		current_states = enemy_states.DEAD
+
+	if player and (global_position.distance_to(player.global_position) <= detection_radius):
+		current_states = enemy_states.CHASE
+	else:
+		if current_states == enemy_states.CHASE:
+			random_generation()
 
 	match current_states:
 		enemy_states.MOVERIGHT:
@@ -32,6 +39,8 @@ func _physics_process(delta):
 			move_down()
 		enemy_states.DEAD:
 			dead()
+		enemy_states.CHASE:
+			chase_player()
 	move_and_slide()
 
 func random_generation():
@@ -64,6 +73,22 @@ func move_up():
 func move_down():
 	velocity = Vector2.DOWN * speed
 	$anim.play("move_down")
+
+func chase_player():
+	if player:
+		dir = (player.global_position - global_position).normalized()
+		velocity = dir * speed
+		if abs(dir.x) > abs(dir.y):
+			if dir.x > 0:
+				$anim.play("move_right")
+			else:
+				$anim.play("move_left")
+		else:
+			if dir.y > 0:
+				$anim.play("move_down")
+			else:
+				$anim.play("move_up")
+
 
 func dead():
 	dead_animation()
